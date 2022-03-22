3c044e4ddf67497ba310fdeed07023370f21beb4
catherine231 <catherineatef23@gmail.com>
2022-03-22 20:34:54 +0200

    starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data,setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.worldweatheronline.com/premium/v1/weather.ashx?key=a80279bb635a4300a1c135839222203&q=${location}&format=json`
+
+
+  const showWeather = (event) => {
+    if(event.key === 'Enter'){
+      axios.get(url).then((response)=> {
+        setData(response.data)
+        console.log(response.data)
+      })
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+      value={location} 
+      type="text" 
+      placeholder='city'
+      onChange={event => setLocation(event.target.value)}
+      onKeyPress={showWeather}
+      >
+      </input>
+      <div>
+        {data.request ? <h1 style={{color:"black"}}>{data.request[0].query}</h1> : null}
+      </div>
     </div>
   );
 }
